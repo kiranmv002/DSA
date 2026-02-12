757aef73da11b68e891991e9765d3a84dd3883f0
M V Kiran <mada.kiran@s.amity.edu>
2026-02-12 21:47:18 +0530

    Add student structure and input/output functionality

--- c_practice/student_structure.c
+++ c_practice/student_structure.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    printf("Enter roll, name, marks: ");
+    scanf("%d %s %f", &s.roll, s.name, &s.marks);
+
+    printf("\nStudent Details:\n");
+    printf("Roll: %d\nName: %s\nMarks: %.2f\n", s.roll, s.name, s.marks);
+
+    return 0;
+}
