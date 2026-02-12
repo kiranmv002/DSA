51a24c1e36712f268a37e090206da34be6782cd9
M V Kiran <mada.kiran@s.amity.edu>
2026-02-12 21:44:23 +0530

    Update reverse_array.c

--- c_practice/reverse_array.c
+++ c_practice/reverse_array.c
@@ -1,1 +1,18 @@
+int main() {
+    int n;
 
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    printf("Reversed array:\n");
+    for(int i=n-1;i>=0;i--)
+        printf("%d ",arr[i]);
+
+    return 0;
+}
