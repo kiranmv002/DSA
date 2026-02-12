a5f779b4206ba2630239b95155ade63a8923b772
M V Kiran <mada.kiran@s.amity.edu>
2026-02-12 20:43:07 +0530

    Update count_digits.c

--- c_practice/count_digits.c
+++ c_practice/count_digits.c
@@ -1,1 +1,15 @@
+int main() {
+    int n, count = 0;
 
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        n /= 10;
+        count++;
+    }
+
+    printf("Total digits: %d\n", count);
+
+    return 0;
+}
