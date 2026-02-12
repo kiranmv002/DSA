a64cb081649fd62ba15db6d98339cdf75013c44a
M V Kiran <mada.kiran@s.amity.edu>
2026-02-12 20:44:09 +0530

    Update armstrong_number.c

--- c_practice/armstrong_number.c
+++ c_practice/armstrong_number.c
@@ -1,1 +1,28 @@
+int main() {
+    int n, original, remainder, result = 0, digits = 0;
 
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (original != 0) {
+        original /= 10;
+        digits++;
+    }
+
+    original = n;
+
+    while (original != 0) {
+        remainder = original % 10;
+        result += pow(remainder, digits);
+        original /= 10;
+    }
+
+    if (result == n)
+        printf("Armstrong number\n");
+    else
+        printf("Not an Armstrong number\n");
+
+    return 0;
+}
