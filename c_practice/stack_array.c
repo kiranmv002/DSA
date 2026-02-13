e14a4ae5f397e2903047b858a524fbe31f09bedd
M V Kiran <mada.kiran@s.amity.edu>
2026-02-13 11:37:17 +0530

    Update stack_array.c

--- c_practice/stack_array.c
+++ c_practice/stack_array.c
@@ -1,1 +1,32 @@
+int stack[SIZE], top = -1;
 
+void push(int value) {
+    if (top == SIZE - 1)
+        printf("Stack Overflow\n");
+    else {
+        stack[++top] = value;
+        printf("Pushed %d\n", value);
+    }
+}
+
+void pop() {
+    if (top == -1)
+        printf("Stack Underflow\n");
+    else
+        printf("Popped %d\n", stack[top--]);
+}
+
+void display() {
+    for (int i = top; i >= 0; i--)
+        printf("%d ", stack[i]);
+    printf("\n");
+}
+
+int main() {
+    push(10);
+    push(20);
+    display();
+    pop();
+    display();
+    return 0;
+}
