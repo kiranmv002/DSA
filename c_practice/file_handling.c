b60b7fddba45290cef8bac73933162df58bab51e
M V Kiran <mada.kiran@s.amity.edu>
2026-02-13 11:38:04 +0530

    Add file handling for student data input/output

--- c_practice/file_handling.c
+++ c_practice/file_handling.c
@@ -1,1 +1,22 @@
 
+int main() {
+    FILE *fptr;
+    char name[50];
+    int marks;
+
+    fptr = fopen("student.txt", "w");
+
+    printf("Enter name and marks: ");
+    scanf("%s %d", name, &marks);
+
+    fprintf(fptr, "%s %d", name, marks);
+    fclose(fptr);
+
+    fptr = fopen("student.txt", "r");
+    fscanf(fptr, "%s %d", name, &marks);
+
+    printf("From file: %s %d\n", name, marks);
+    fclose(fptr);
+
+    return 0;
+}
