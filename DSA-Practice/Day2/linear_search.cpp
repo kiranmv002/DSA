762d8755ea83b2042538173990ce26982579267a
M V Kiran <mada.kiran@s.amity.edu>
2026-08-14 21:28:14 +0530

    Update linear_search.cpp

--- DSA-Practice/Day2/linear_search.cpp
+++ DSA-Practice/Day2/linear_search.cpp
@@ -1,6 +1,6 @@
 int main() {
-    int arr[] = {10,20,30,40};
-    int n = 4, key = 30;
+    int arr[] = {10,20,30,40,50};
+    int n = 5, key = 30;
     int pos = -1;
 
     for(int i = 0; i < n; i++) {
