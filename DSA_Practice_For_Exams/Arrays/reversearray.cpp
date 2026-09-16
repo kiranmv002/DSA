2cf94c7a62c67f1855ecebcd465a83fcd6ad8e1d
M V Kiran <mada.kiran@s.amity.edu>
2026-09-16 22:18:39 +0530

    Update reversearray.cpp

--- DSA_Practice_For_Exams/Arrays/reversearray.cpp
+++ DSA_Practice_For_Exams/Arrays/reversearray.cpp
@@ -1,7 +1,7 @@
 // Reverse array
 int main() {
-    int arr[] = {1,2,3,4,5};
-    int n = 5;
+    int arr[] = {1,2,3,4,5,6};
+    int n = 6;
 
     for(int i = 0; i < n/2; i++) {
         swap(arr[i], arr[n-i-1]);
